300f04a4f859d77146f6293e61f610f29826b332
FNP191 <facundonoble191@gmail.com>
2026-09-16 10:15:59 -0300

    Arreglo de Coyote en clase

--- the-little-kay-slyme/entities/player/player.gd
+++ the-little-kay-slyme/entities/player/player.gd
@@ -6,9 +6,10 @@ class_name Player
 @export var jump_velocity : float = -250
 @export var acceleration : float = 25
 @export var friction : float = 15
-@export var max_jumps: int = 2
+@export var max_jumps: int =1
 @export var max_hp: int = 3
 @export var repair_time: float = 2
+@export var coyote_time: float = 0.2
 
 @export_group("Components")
 @export var sound_list: Dictionary[String, AudioStream]
@@ -35,17 +36,20 @@ var is_suspended = false
 var time_repairing = 0
 var time_in_idle = 0
 var knockback = Vector2.ZERO
+var time_in_state : float
+var last_state:States = States.IDLE
+var jump_pressed: bool = false
 
 func jump():
-	if jumps_performed < 1 and is_on_floor():
-		velocity.y = lerp(velocity.y, jump_velocity, 1)
-		player_sprites.scale = Vector2(0.7, 1.3)
-		jumps_performed += 1
-	elif jumps_performed >= 1 or not is_on_floor():
-		velocity.y = lerp(velocity.y, jump_velocity, 1)
-		player_sprites.scale = Vector2(0.7, 1.3)
-		jumps_performed += 1
+	velocity.y = lerp(velocity.y, jump_velocity, 1)
+	player_sprites.scale = Vector2(0.7, 1.3)
+	jumps_performed += 1
+	if jumps_performed < 1 and can_double_jump():
 		player_jump_particles.restart()
+		
+func can_double_jump()-> bool:
+	return not is_on_floor() and jumps_performed < max_jumps
+	
 func move():
 	velocity.x = lerp(velocity.x, round(direction) * max_speed, lerp_weight)
 
@@ -76,20 +80,28 @@ func reset_scale():
 		player_sprites.scale.x = move_toward(player_sprites.scale.x, 1, 2 * get_physics_process_delta_time())
 		player_sprites.scale.y = move_toward(player_sprites.scale.y, 1, 2 * get_physics_process_delta_time())
 
+
 func set_state():
+	var new_state := current_state
+
 	if velocity.y < 0:
-		current_state = States.JUMPING
-	if velocity.y > 0:
-		current_state = States.FALLING
-	if is_on_floor() and not direction:
-		current_state = States.IDLE
-	if is_on_floor() and round(direction):
-		current_state = States.WALKING
+		new_state = States.JUMPING
+	elif velocity.y > 0:
+		new_state = States.FALLING
+	elif is_on_floor():
+		new_state = States.WALKING if round(direction) else States.IDLE
+
+	if new_state != current_state:
+		last_state = current_state
+		current_state = new_state
+		time_in_state = 0.0
+
 	if round(direction):
 		player_sprites.flip_h = ceil(direction) - 1
 
 func handle_states():
 	set_state()
+	time_in_state += get_physics_process_delta_time()
 	match current_state:
 		States.IDLE:
 			player_smoke_particles.emitting = false
@@ -102,10 +114,14 @@ func handle_states():
 				player_animator.play("Blink", -1, 0.5)
 			else:
 				player_animator.play("Sleep", -1, 0.5)
+			if jump_pressed and is_on_floor():
+				jump()
 		States.WALKING:
 			player_smoke_particles.emitting = true
 			time_in_idle = 0
 			player_animator.play("Walk", -1, 2)
+			if jump_pressed and is_on_floor():
+				jump()
 		States.JUMPING:
 			player_smoke_particles.emitting = false
 			time_in_idle = 0
@@ -113,26 +129,32 @@ func handle_states():
 			is_suspended = true
 			if Input.is_action_just_released("Jump"):
 				velocity.y = lerp(velocity.y, jump_velocity/4, 1)
+			if jump_pressed and can_double_jump():
+				jump()
 		States.FALLING:
 			player_smoke_particles.emitting = false
 			time_in_idle = 0
+			if jump_pressed and time_in_state <= coyote_time and last_state != States.JUMPING:
+				print("coyote")
+				jump()
+			if jump_pressed and can_double_jump():
+				jump()
 			player_animator.play("Fall", -1, 1.5)
 			is_suspended = true
+		
 
 func _ready() -> void:
 	last_respawn_pos = position
-	hp_bar.text = "Vidas: {0}".format([current_hp])
+#	hp_bar.text = "Vidas: {0}".format([current_hp])
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_axis("MoveLeft","MoveRight")
+	jump_pressed = Input.is_action_just_pressed("Jump")
 	lerp_weight = delta * (acceleration if direction else friction)
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	elif is_on_floor():
 		jumps_performed = 0
-	
-	if Input.is_action_just_pressed("Jump") and jumps_performed < max_jumps:
-		jump()
 	
 	if Input.is_action_pressed("RepairAction"):
 		repair()
